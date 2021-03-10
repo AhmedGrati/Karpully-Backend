848dc1ab51a867e49d037ba812a4d14a88c4b344
AhmedGrati <ahmedgrati1999@gmail.com>
2021-03-10 10:20:05 +0100

    config git hooks

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'airbnb-base',
+    'prettier'
+  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es6:true
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': [
+      "error",
+      {
+        "vars": "local",
+        "args": "none"
+      }
+    ],
+    'no-plusplus': "off",
+    'no-underscore-dangle': "off",
+  }
+};
