5db243f74be3464e7dd1b0deb32b8e9456576f9d
AhmedGrati <ahmedgrati1999@gmail.com>
2021-04-16 08:14:30 +0100

    add submission dto

--- src/submission/dto/create-submission.input.ts
+++ src/submission/dto/create-submission.input.ts
@@ -2,6 +2,8 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateSubmissionInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+
+  @Field()
+  carpoolId: number;
+
 }
