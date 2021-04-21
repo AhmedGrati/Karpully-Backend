cf770cd9f276ab50be7675a09bf28445267e300b
AhmedGrati <ahmedgrati1999@gmail.com>
2021-04-21 17:10:50 +0100

    add carpool unit test

--- src/carpool/carpool.resolver.spec.ts
+++ src/carpool/carpool.resolver.spec.ts
@@ -1,19 +1,87 @@
 import {Test, TestingModule} from '@nestjs/testing';
+import {User} from '../user/entities/user.entity';
+import {PaginationInput} from '../generics/pagination.input';
 import {CarpoolResolver} from './carpool.resolver';
 import {CarpoolService} from './carpool.service';
+import {Where} from './dto/where.input';
+import {Carpool} from './entities/carpool.entity';
+import {PaginatedCarpool} from './entities/paginatedCarpool.entity';
+import {CreateCarpoolInput} from './dto/create-carpool.input';
 
 describe('CarpoolResolver', () => {
   let resolver: CarpoolResolver;
-
+  const carpoolMockService = {
+    paginatedCarpools: jest.fn().mockImplementation(
+      (paginationInput, where?): PaginatedCarpool => {
+        return {meta: {itemCount: 0, currentPage: 1}, items: []};
+      },
+    ),
+    restoreCarpool: jest.fn().mockImplementation((user, id) => new Carpool()),
+    remove: jest.fn().mockImplementation((user, id) => new Carpool()),
+    findOne: jest.fn().mockImplementation((id) => {
+      return {id};
+    }),
+    create: jest.fn().mockImplementation((dto) => {
+      return {
+        ...dto,
+        id: 1,
+      };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CarpoolResolver, CarpoolService],
-    }).compile();
+    })
+      .overrideProvider(CarpoolService)
+      .useValue(carpoolMockService)
+      .compile();
 
     resolver = module.get<CarpoolResolver>(CarpoolResolver);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should return a pagination of carpools', () => {
+    const where: Where = {
+      id: 1,
+    };
+    const paginationInput: PaginationInput = {
+      limit: 20,
+      page: 2,
+    };
+    expect(resolver.paginatedCarpool(paginationInput, where)).toBeDefined();
+    expect(carpoolMockService.paginatedCarpools).toBeCalledWith(
+      paginationInput,
+      where,
+    );
+  });
+  it('should restore a carpool with the specific id', () => {
+    const id: number = 1;
+    const user: User = new User();
+    expect(resolver.restoreCarpool(user, id)).toBeDefined();
+    expect(carpoolMockService.restoreCarpool).toBeCalledWith(user, id);
+  });
+
+  it('should remove a carpool with the specific id', () => {
+    const id: number = 1;
+    const user: User = new User();
+    expect(resolver.removeCarpool(user, id)).toBeDefined();
+    expect(carpoolMockService.remove).toBeCalledWith(user, id);
+  });
+
+  it('should find a carpool with the specific id', () => {
+    const id: number = 1;
+    expect(resolver.findOneCarpool(id)).toMatchObject({id});
+    expect(carpoolMockService.findOne).toBeCalledWith(id);
+  });
+  it('should find a carpool with the specific id', () => {
+    const createCarpoolInput: CreateCarpoolInput = {} as CreateCarpoolInput;
+    const user = new User();
+    expect(resolver.createCarpool(user, createCarpoolInput)).toMatchObject({
+      id: 1,
+    });
+    expect(carpoolMockService.create).toBeCalled();
   });
 });
--- src/carpool/carpool.service.spec.ts
+++ src/carpool/carpool.service.spec.ts
@@ -1,18 +1,50 @@
 import {Test, TestingModule} from '@nestjs/testing';
+import {getRepositoryToken} from '@nestjs/typeorm';
+import {User} from '../user/entities/user.entity';
+import {CaslAbilityFactory, Subjects} from '../casl/casl-ability.factory';
+import {CityService} from '../city/city.service';
 import {CarpoolService} from './carpool.service';
+import {Carpool} from './entities/carpool.entity';
+import {Ability} from '@casl/ability';
 
 describe('CarpoolService', () => {
   let service: CarpoolService;
-
+  const mockCarpoolRepository = {
+    restore: jest.fn().mockImplementation((user, id) => {
+      return Promise.resolve({id});
+    }),
+    findOne: jest.fn().mockImplementation((id) => {
+      return Promise.resolve({id});
+    }),
+  };
+  const mockCityService = {};
+  const mockCaslFactory = {};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CarpoolService],
-    }).compile();
+      providers: [
+        CarpoolService,
+        {
+          provide: getRepositoryToken(Carpool),
+          useValue: mockCarpoolRepository,
+        },
+        CityService,
+        CaslAbilityFactory,
+      ],
+    })
+      .overrideProvider(CityService)
+      .useValue(mockCityService)
+      .overrideProvider(CaslAbilityFactory)
+      .useValue(mockCaslFactory)
+      .compile();
 
     service = module.get<CarpoolService>(CarpoolService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return the paginated carpools', async () => {
+    expect(await service.paginatedCarpools).toBeDefined();
   });
 });
