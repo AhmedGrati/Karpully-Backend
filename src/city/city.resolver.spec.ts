d4c34c745152a6f2c24faf95b01effa911335ecb
AhmedGrati <ahmedgrati1999@gmail.com>
2021-04-23 16:23:03 +0100

    add city unit tests

--- src/city/city.resolver.spec.ts
+++ src/city/city.resolver.spec.ts
@@ -1,19 +1,35 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {CityResolver} from './city.resolver';
 import {CityService} from './city.service';
+import {City} from './entities/city.entity';
 
+/*
+  The most used one is findOneCity so we will only test it
+*/
 describe('CityResolver', () => {
   let resolver: CityResolver;
-
+  const city = new City();
+  const mockCityService = {
+    findOne: jest.fn().mockReturnValue(city),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CityResolver, CityService],
-    }).compile();
+    })
+      .overrideProvider(CityService)
+      .useValue(mockCityService)
+      .compile();
 
     resolver = module.get<CityResolver>(CityResolver);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should find a city with the specific id', () => {
+    expect(resolver.findOneCity(1)).toBeDefined();
+    expect(resolver.findOneCity(1)).toMatchObject(city);
+    expect(mockCityService.findOne).toBeCalledWith(1);
   });
 });
--- src/city/city.service.spec.ts
+++ src/city/city.service.spec.ts
@@ -1,18 +1,41 @@
 import {Test, TestingModule} from '@nestjs/testing';
+import {getRepositoryToken} from '@nestjs/typeorm';
+import {GovService} from '../gov/gov.service';
 import {CityService} from './city.service';
+import {City} from './entities/city.entity';
 
 describe('CityService', () => {
   let service: CityService;
-
+  const city = new City();
+  const mockCityRepository = {
+    findOne: jest.fn().mockResolvedValue(city),
+  };
+  const mockGovService = {};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CityService],
-    }).compile();
+      providers: [
+        CityService,
+        {
+          provide: getRepositoryToken(City),
+          useValue: mockCityRepository,
+        },
+        GovService,
+      ],
+    })
+      .overrideProvider(GovService)
+      .useValue(mockGovService)
+      .compile();
 
     service = module.get<CityService>(CityService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a city with the specific id', async () => {
+    expect(await service.findOne(1)).toBeDefined();
+    expect(await service.findOne(1)).toMatchObject(city);
+    expect(mockCityRepository.findOne).toBeCalled();
   });
 });
