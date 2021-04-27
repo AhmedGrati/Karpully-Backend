94183219798b68c1f447640e40a7bd3f2d4856f0
AhmedGrati <ahmedgrati1999@gmail.com>
2021-04-27 15:25:30 +0100

    basic deploy

---
+++ terraform/modules/k8s/k8s.tf
@@ -0,0 +1,69 @@
+provider "kubernetes" {
+    host                   =  var.host
+    client_certificate     =  var.client_certificate
+    client_key             =  var.client_key
+    cluster_ca_certificate =  var.cluster_ca_certificate
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "serviceprinciple_id" {
+}
+
+variable "serviceprinciple_key" {
+}
+
+variable "tenant_id" {
+}
+
+variable "subscription_id" {
+}
+
+
+variable "ssh_key" {
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "kubernetes_version" {
+    default = "1.20.5"
+}
