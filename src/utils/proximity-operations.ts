75f48d70cdd1a8e64c524a7da295b68e2d73b0c5
Mellywins <38540621+Mellywins@users.noreply.github.com>
2021-05-10 02:40:25 +0100

    writing utility functions

---
+++ src/utils/proximity-operations.ts
@@ -0,0 +1,21 @@
+import haversine from 'haversine-distance';
+const calculateViewBox = (lon: number, lat: number, radius: number): {
+    minLat: number,
+    maxLat: number,
+    minLon: number,
+    maxLon: number
+} => {
+    const KmInOneLongitude = 111.320 * Math.cos(lat / 180.0 * Math.PI);
+    const deltaLat = radius / 111.1;
+    const deltaLong = radius / KmInOneLongitude;
+    const minLat = lat - deltaLat;
+    const maxLat = lat + deltaLat;
+    const minLon = lon - deltaLong;
+    const maxLon = lon + deltaLong;
+    return {
+        minLat, maxLat, minLon, maxLon
+    }
+}
+export {
+    calculateViewBox, haversine
+}
