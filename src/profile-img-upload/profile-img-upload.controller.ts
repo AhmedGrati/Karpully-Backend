b00565bc5d72a65ae8c0f15613f65b917d960ede
Mellywins <38540621+Mellywins@users.noreply.github.com>
2021-06-12 11:37:09 +0100

    webp images support

--- src/profile-img-upload/profile-img-upload.controller.ts
+++ src/profile-img-upload/profile-img-upload.controller.ts
@@ -28,7 +28,7 @@ export class ProfileImgUploadController {
         }
       }),
       fileFilter: (req, file, callback) => {
-        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif|webp)$/)) {
           return callback(new Error('Only image files are allowed!'), false);
         }
         callback(null, true);
